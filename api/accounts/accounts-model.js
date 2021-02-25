7a9b948d67eb0bbd210b20f63573827cbfcaa2aa
Amanda Nelson <amanda.gaylord@gmail.com>
2021-02-25 00:12:59 -0500

    accounts-model.js updated.

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,37 @@
+const db = require("../data/db-config.js")
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db("accounts")
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
+  return db("accounts").where("id", id).first();
 }
 
 const create = async account => {
-  // DO YOUR MAGIC
+  return await db("accounts")
+  .insert(account)
+  .then((id) => {
+    return db("accounts").where("id", id).first();
+  })
 }
 
 const updateById = async (id, account) => {
-  // DO YOUR MAGIC
+  return await db("accounts")
+  .where("id", id)
+  .update(account)
+  .then((id) => {
+    return db("accounts").where("id", id).first()
+  })
 }
 
 const deleteById = async id => {
-  // DO YOUR MAGIC
+  return await db("accounts")
+  .where("id", id)
+  .del()
+  .then(() => {
+    return db("accounts")
+  })
 }
 
 module.exports = {

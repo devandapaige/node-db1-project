12d584d1f51fd012ad79ec75b5c1c4c8e9ce27b8
Amanda Nelson <amanda.gaylord@gmail.com>
2021-03-08 00:13:14 -0500

    mvp I never pushed

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,38 +1,47 @@
-const db = require("../data/db-config.js")
+const knex = require(knex);
+const config = {
+  client: "sqlite3",
+  connection: {
+    filename: ".../data/budget.db3",
+  },
+  useNullAsDefault: true,
+};
+
+const db = knex(config);
 
 const getAll = () => {
-  return db("accounts")
-}
+  return db("Accounts");
+};
 
-const getById = id => {
-  return db("accounts").where("id", id).first();
-}
+const getById = (id) => {
+  return db("Accounts").where("id", id).first();
+};
 
-const create = async account => {
-  return await db("accounts")
-  .insert(account)
-  .then((id) => {
-    return db("accounts").where("id", id).first();
-  })
-}
+const create = async (account) => {
+  return await db("Accounts")
+    .insert(account)
+    .then((id) => {
+      return db("Accounts").where("id", id).first();
+    });
+};
 
 const updateById = async (id, account) => {
-  return await db("accounts")
-  .where("id", id)
-  .update(account)
-  .then((id) => {
-    return db("accounts").where("id", id).first()
-  })
-}
+  return await db("Accounts")
+    .where("id", id)
+    .update(account)
+    .then((id) => {
+      return db("Accounts").where("id", id).first();
+    });
+};
 
-const deleteById = async id => {
+const deleteById = async (id) => {
   return await db("accounts")
-  .where("id", id)
-  .del()
-  .then(() => {
-    return db("accounts")
-  })
-}
+    .where("id", id)
+    .del()
+    .then(() => {
+      return db("accounts");
+    });
+};
 
 module.exports = {
   getAll,
@@ -40,4 +49,4 @@ module.exports = {
   create,
   updateById,
   deleteById,
-}
+};
